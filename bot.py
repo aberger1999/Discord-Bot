813e910785be32c8058da1eeb2e0166e9e9fc8ee
Alex Berger <97977620+aberger1999@users.noreply.github.com>
2025-01-28 14:56:04 -0500

    fix

--- bot.py
+++ bot.py
@@ -50,25 +50,6 @@ async def on_ready():
     print("Ready!")
 
 
-
-##client = commands.Bot(command_prefix="=", intents=discord.Intents.all())
-
-'''@client.event
-async def on_ready():
-    print("Success: Bot is connected to Discord")
-
-@client.command()
-async def ping(ctx):
-    await ctx.send("Ping Pong Fucker!")
-
-@client.command(aliases=["8ball", "eightball", "eight ball"])
-async def magic_eightball(ctx, *, question):
-    with open("discordbot/response.txt", "r") as f:
-        random_response = f.readlines()
-        response = random.choice(random_response)
-
-    await ctx.send(response)'''
-
 client.run(TOKEN)
 
 
