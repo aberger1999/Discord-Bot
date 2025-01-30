2308ec215bb9db3067814f948934b946bcae662d
Alex Berger <aberger1999@gmail.com>
2025-01-30 11:53:57 -0500

    Moved to folder

--- Bot/bot.py
+++ Bot/bot.py
@@ -7,7 +7,7 @@ from PIL import Image
 from datetime import datetime
 from giphy_client.rest import ApiException
 from googleapiclient.discovery import build
-from config import TOKEN, GIPHY_API_KEY, GOOGLE_API_KEY, GOOGLE_CSE_ID
+from Bot.config import TOKEN, GIPHY_API_KEY, GOOGLE_API_KEY, GOOGLE_CSE_ID
 
 
 intents = discord.Intents.default()
